68e88a98a657f15ee788893a2729abcae275a402
ChrisvanderHorst0308 <112861116+ChrisvanderHorst0308@users.noreply.github.com>
2022-09-19 10:54:26 +0200

    Add files via upload

---
+++ your-tribe-profile-card/your-tribe-profile-card/scripts/script.js
@@ -0,0 +1,8 @@
+const expandButton = document.querySelector('header button')
+expandButton.addEventListener('click', expand)
+
+function 
+
+// function expand () {
+//   document.body.classList.toggle('expand')
+}
